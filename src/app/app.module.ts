4bd5e50c00af492f4a74de105589f81aae52a634
ArmandoDiazDiaz <armando.diaz.diaz.2409@gmail.com>
2020-07-17 18:41:29 -0500

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { VersusComponent } from './pages/versus/versus.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TorneosComponent } from './pages/torneos/torneos.component';
+import { TiendaComponent } from './pages/tienda/tienda.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    VersusComponent,
+    HomeComponent,
+    TorneosComponent,
+    TiendaComponent
   ],
   imports: [
     BrowserModule,
